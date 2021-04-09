953e2da4fb8b9d537f3388fa3edcacccc8cd8778
Ty Foster <tyfoster@outlook.com>
2021-04-09 12:50:35 -0700

    Adds documentation

--- src/events/error.ts
+++ src/events/error.ts
@@ -1,7 +1,20 @@
 import { Client } from 'discord.js';
 import { errLog } from '../utils/logs';
+/**
+ * @file error.ts
+ * @author Ty Foster
+ * @version 2021.04.08
+ * 
+ * Handles Discord Client MessageEvent
+ */
 
+/**
+ * Processes error for the Bot
+ * 
+ * @param client the Client for the Bot
+ * @param message the Error to process
+ */
 module.exports = (client: Client, error: Error) => {
   console.log(error);
   errLog(client, error, null);
-}
+};
