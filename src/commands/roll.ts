b5529749373d5487c1d68d308a0a276e9b8af031
Ty Foster <tyfoster@outlook.com>
2021-04-09 12:52:02 -0700

    Adds documentation and ignores run method for coverage report

--- src/commands/roll.ts
+++ src/commands/roll.ts
@@ -15,14 +15,21 @@ import { h_roll } from './help';
  * @param message a Discord Message sent by a User
  * @param args an array of string arguments from the Message
  */
+/* istanbul ignore next */ // cannot test with jest
 module.exports.run = async (client: Client, message: Message, args: string[]) => {
-  if (args.length != 1 || isNaN(Number(args[0]))) {
+  if (args.length != 1 || isNaN(Number(args[0]))) { // screen for correct number of args and ability to convert
     message.channel.send(h_roll());
-  } else {
+  } else { // handle happy day case
     message.reply(` you rolled a ${roll(Number(args[0]))}`);
   }
 };
 
+/**
+ * Simulates rolling a die
+ * 
+ * @param sides the number of sides for the die
+ * @returns the roll of the die
+ */
 export function roll (sides: number): number {
   if (sides <= 0) {
     sides = 0;
